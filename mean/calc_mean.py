8f505b341fe0250249471f0923102af57c375753
Punvireakroth <vireakrothpun@gmail.com>
2022-12-26 21:11:26 +0700

    calc mean

---
+++ mean/calc_mean.py
@@ -0,0 +1,18 @@
+# total observations
+
+total = 0
+
+mean_value = 0
+
+age = [29,49,42,43]
+
+sum_element = 0
+for i in age:
+    sum_element += i
+
+
+mean_value = sum_element / len(age)
+
+# answer 
+print("The sum total is equal to: " + str(total))
+print("The mean value is equal to: " + str(mean_value))
