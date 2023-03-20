61c0a417bcbf468546e2a7c21a52b2248e69e130
Punvireakroth <vireakrothpun@gmail.com>
2023-03-20 21:33:14 +0700

    initial commit

---
+++ Lab6/run.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template
+app=Flask(__name__,template_folder='templates')
+app.debug = True
+
+@app.route("/")
+def helloWorld():
+    return "<p>Hello, World!</p>"
+
+@app.route("/home")
+def home():
+    return render_template("test.html")
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+
+@app.route("/image")
+def image():
+    return render_template("image.html")
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8080)
